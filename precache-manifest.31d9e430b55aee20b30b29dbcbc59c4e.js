5f71e1666d1eca3c54469ca12d89c5a4cf7db46b
Kamil <kamilobinski@gmail.com>
2020-03-30 01:37:30 +0200

    Updates

---
+++ precache-manifest.31d9e430b55aee20b30b29dbcbc59c4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df82a65c1498f071fad68b5ec1d5346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd0aee54f8af1ff5c73e",
+    "url": "/static/css/main.786a271e.chunk.css"
+  },
+  {
+    "revision": "cfaf5ce94d08748f1dd4",
+    "url": "/static/js/2.8160693a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8160693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0aee54f8af1ff5c73e",
+    "url": "/static/js/main.31858d94.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "f0d9e93231e05388cbfb9482c4b3643a",
+    "url": "/static/media/github.f0d9e932.svg"
+  },
+  {
+    "revision": "806d56e67768c0efdfec7c38d60a9644",
+    "url": "/static/media/linkedin.806d56e6.svg"
+  },
+  {
+    "revision": "31b85012178a9bccc6af6afacce29713",
+    "url": "/static/media/twitter.31b85012.svg"
+  }
+]);

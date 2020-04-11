cccccb731ed8650c1cfddc79e8a5113be7cc5836
Kamil <kamilobinski@gmail.com>
2020-04-11 19:04:11 +0200

    Improved svg rendering, added hover event

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ChangeTheme from "./ChangeTheme";
-import github from "../utils/icons/github.svg";
-import linkedin from "../utils/icons/linkedin.svg";
-import twitter from "../utils/icons/twitter.svg";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Github, Linkedin, Twitter, Gmail } from "./icons/SocialIcons";
 import "../style/SocialMedia.css";
 
 const githubUrl = "https://github.com/kamillobinski";
 const linkedinUrl = "https://www.linkedin.com/in/kamillobinski/?locale=en_US";
 const twitterUrl = "https://twitter.com/kamillobinski";
+const gmailAccount = "kamilobinski@gmail.com";
 
-const SocialMedia = () => {
-  return (
-    <div className="social-media">
-      <a href={githubUrl} target="_blank" rel="noopener noreferrer">
-        <img src={github} className="social-media-icon" alt="github" />
-      </a>
-      <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
-        <img src={linkedin} className="social-media-icon" alt="linkedin" />
-      </a>
-      <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
-        <img src={twitter} className="social-media-icon" alt="twitter" />
-      </a>
-      <ChangeTheme />
-    </div>
-  );
-};
+class SocialMedia extends React.Component {
+  render() {
+    return (
+      <div className="social-media">
+        <a href="mailto: kamilobinski@gmail.com">
+          <CopyToClipboard text={gmailAccount}>
+            <span>
+              <Gmail width="18" height="18" />
+            </span>
+          </CopyToClipboard>
+        </a>
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+          <Github width="18" height="18" />
+        </a>
+        <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <Linkedin width="18" height="18" />
+        </a>
+        <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
+          <Twitter width="18" height="18" />
+        </a>
+        <ChangeTheme />
+      </div>
+    );
+  }
+}
 
 export default SocialMedia;

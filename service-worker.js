96b76364279308b6f7e90fde67fad2bb253a3a00
Kamil <kamilobinski@gmail.com>
2020-05-01 01:15:12 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03decade30f9d022df9474b7f2f6cd05.js"
+  "/precache-manifest.7fc6994d938f2319dd01e47c2a335d61.js"
 );
 
 self.addEventListener('message', (event) => {

a6b8e1eea52a134ee8a25e7ff6912eec93d028b6
Kamil <kamilobinski@gmail.com>
2020-05-05 01:19:38 +0200

    Changed theme button

--- src/components/ChangeTheme.js
+++ src/components/ChangeTheme.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ThemeDark, ThemeLight } from "./icons/ThemeIcons";
 import "../utils/global.css";
 import "../style/ChangeTheme.css";
 
@@ -6,12 +7,19 @@ class ChangeTheme extends React.Component {
   state = {
     checked: localStorage.getItem("theme") === "dark" ? true : false,
     theme: localStorage.getItem("theme"),
+    isDarkThemeEnabled: false,
   };
 
   componentDidMount() {
     document
       .getElementsByTagName("HTML")[0]
       .setAttribute("data-theme", localStorage.getItem("theme"));
+    console.log(this.state.checked);
+    if (this.state.checked === true) {
+      this.setState({ isDarkThemeEnabled: true });
+    } else if (this.state.checked === false) {
+      this.setState({ isDarkThemeEnabled: false });
+    }
   }
 
   toggleThemeChange = () => {
@@ -24,6 +32,7 @@ class ChangeTheme extends React.Component {
       this.setState({
         checked: true,
       });
+      this.setState({ isDarkThemeEnabled: true });
     } else {
       localStorage.setItem("theme", "light");
       document
@@ -32,13 +41,21 @@ class ChangeTheme extends React.Component {
       this.setState({
         checked: false,
       });
+      this.setState({ isDarkThemeEnabled: false });
     }
   };
 
   render() {
     return (
       <div className="switch-outer">
-        <span>Dark mode </span>
+        <span
+          className="switch-outer-icon-light"
+          style={{
+            fill: this.state.isDarkThemeEnabled ? "#7a7c80" : "#932eff",
+          }}
+        >
+          <ThemeLight width="13px" height="13px" />
+        </span>
         <label className="switch">
           <input
             type="checkbox"
@@ -47,6 +64,16 @@ class ChangeTheme extends React.Component {
           />
           <span className="slider round" />
         </label>
+        <span
+          className="switch-outer-icon-dark"
+          style={{
+            fill: this.state.isDarkThemeEnabled ? "#932eff" : "#7a7c80",
+          }}
+        >
+          {" "}
+          {console.log("test: " + this.state.isDarkThemeEnabled)}
+          <ThemeDark width="13px" height="13px" />
+        </span>
       </div>
     );
   }
